e45453ce2f8fa942a97e7203a099d410e5bdb6eb
Zoinloz <blanckevin19@gmail.com>
2019-12-09 17:31:57 +0100

    Connexion + Insert + Lecture MongoDB

--- Tp.js
+++ Tp.js
@@ -5,6 +5,43 @@ const http = require('http').createServer(app);
 const path = require('path'); //adapter le chemin d'acces en fonction de l'OS
 const io = require('socket.io')(http);
 
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = 'mongodb://localhost:27017/Node';
+
+//Instancie un objet de type MongoClient pour acceder aux fonctions et objets
+let maVar = new MongoClient(url, {});
+
+// Use connect method to connect to the Server
+maVar.connect(function (err) {
+
+    //Code qui exploite la BDD si elle n'existe pas il crée la BDD
+    let db = maVar.db('Node')
+
+    //Code qui exploite la collection
+    let collection = db.collection("Chat");
+
+    //Insertion = INSERT dans la collection
+    // collection.insert({
+
+    //     message: 'Mon Message',
+    //     created: '2019-12-10T14:36:00.000+00:00' //Date de l'envoie du message
+
+    // }, function (err) {
+    //     if (!err) console.log('Chat Inserted !')
+    // })
+
+    //Find = SELECT dans la collection
+collection.find({}).toArray(function(err, result) {
+    if (!err) console.log(result);
+});
+
+console.log("Connected correctly to server");
+});
+
+
+
 //Fonction static
 app.use(express.static(path.join(__dirname, 'express'))); // recupere l'index.html dans le dossier client
 
@@ -17,24 +54,24 @@ app.get('/', (req, res) => {
 
 io.on('connection', client => {
 
-                userNbr++;
-                console.log(userNbr);
-                io.sockets.emit('userConnect', userNbr);
-    
-                //Déconnexion d'un utilisateur quand il ferme la page et donc perd la connexion au serveur
-                client.on('disconnect', () => {
-                    userNbr--;
-                    console.log(userNbr);
-                    io.sockets.emit('userDisconnect', userNbr);
-                })
-    
-                //Envoie une socket à tous les clients présent sur le site avec le message q'un utilisateur a envoyé
-                client.on('envoyer', function recevoirMessage(message) {
-                    console.log(message);
-                    var monMessage = message;
-                    io.sockets.emit('message', monMessage);
-                });
-            });
+    userNbr++;
+    console.log(userNbr);
+    io.sockets.emit('userConnect', userNbr);
+
+    //Déconnexion d'un utilisateur quand il ferme la page et donc perd la connexion au serveur
+    client.on('disconnect', () => {
+        userNbr--;
+        console.log(userNbr);
+        io.sockets.emit('userDisconnect', userNbr);
+    })
+
+    //Envoie une socket à tous les clients présent sur le site avec le message q'un utilisateur a envoyé
+    client.on('envoyer', function recevoirMessage(message) {
+        console.log(message);
+        var monMessage = message;
+        io.sockets.emit('message', monMessage);
+    });
+});
 
 http.listen(3001);
 
